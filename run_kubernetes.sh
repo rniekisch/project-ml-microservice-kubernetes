1b1d48484eca032c419391e7188be50fb360dcb5
Roland Niekisch <roland.niekisch@t-online.de>
2021-05-30 20:17:48 +0200

    Task 7 Run kuberentes cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
+# This runs an image in a kubernetes cluster
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="rniekisch/udacity_dev_ops:api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run api\
+    --image=$dockerpath\
+    --port=80 --labels app=project5
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl wait --for=condition=ready pod -l app=project5 
+kubectl port-forward api 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=rniekisch/udacity_dev_ops:api
 
 # Step 2:  
 # Authenticate & tag
+docker login -u rniekisch
+docker tag api $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
